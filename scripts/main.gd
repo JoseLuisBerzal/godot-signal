158f70c08bfb70ff5bd2898c9e7c55953b168702
Berzal <educa@berzal.es>
2023-11-26 14:57:55 +0100

    Added comment

--- scripts/main.gd
+++ scripts/main.gd
@@ -1,12 +1,12 @@
 extends Control
 
-@onready var btn_pause = $btn_pause
+@onready var btn_pause = $btn_pause  # Instanciamos el botón.
 
 func _ready():
 	pass 
 
 func _on_control_godot_pause(isPaused):
-	if isPaused:
-		btn_pause.text = "Restart"
-	else:
-		btn_pause.text = "Pause"
+	if isPaused: # Si el juego está pausado ...
+		btn_pause.text = "Restart" # ... cambiamos el texto del botón a Restart
+	else: # Si el juego no está pausado...
+		btn_pause.text = "Pause" # ... cambiamos el texto del botón a Pause
--- scripts/timer.gd
+++ scripts/timer.gd
@@ -1,25 +1,27 @@
 extends Control
 
-@onready var timer = $timer
-@onready var lbl_timer = $timer/lbl_timer
+@onready var timer = $timer  # Instanciamos el Timer.
+@onready var lbl_timer = $timer/lbl_timer # Instanciamos el Label.
 
-signal godot_pause(isPaused)
-var seconds
+signal godot_pause(isPaused) # Con esto creamos un señal custom.
+var seconds # Variable para guardar el segundo actual.
 
 func _ready():
-	seconds = 0
-	timer.start()
+	seconds = 0   # Le indicamos que segundo es el primero.
+	timer.start() # Arrancamos el Timer (contador)
 
-func _on_timer_timeout():
-	seconds += 1
-	lbl_timer.text = str(seconds)
+func _on_timer_timeout():  
+	seconds += 1 # Le sumamos 1 segundo al 'reloj'
+	lbl_timer.text = str(seconds) # Y visualizamos es
 
 func _on_btn_pause_pressed():
-	if timer.paused:
-		print("Restart")
-		timer.set_paused(false)
-		emit_signal("godot_pause", false)
-	else:
-		print("Pause")
-		timer.set_paused(true)
+	if timer.paused: # Si el estado del timer está pausado ...
+		print("Restart") # ... visualizamos en consola Restart ...
+		timer.set_paused(false) # ... quitamos la pausa del timer ...
+		# ... y emitimos la señal godot_pause con el parámetro a false
+		emit_signal("godot_pause", false) 
+	else: # Si el estado del timer no es pausado ...
+		print("Pause") # ... visualizamos en consola Pause ...
+		timer.set_paused(true) # ... iniciamos la pausa del timer ...
+		# ... y emitimos la señal godot_pause con el parámetro a true
 		emit_signal("godot_pause", true)
